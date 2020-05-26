2d365ed2f315a3766047cd2929e2b40623d775f2
Boukhemis Dib <boukhemis2011@gmail.com>
2020-05-26 10:40:23 +0200

    config bdd MySQL

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
-  imports: [],
+  imports: [
+    //Lorsque vous souhaitez l'utiliser ConfigModuledans d'autres modules il faut mettre {isGlobal: true}
+    ConfigModule.forRoot({isGlobal: true}),
+    //Config bdd MySQL
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT, 1000) || 3308,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [],
+      autoLoadEntities:true,
+      synchronize: true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
